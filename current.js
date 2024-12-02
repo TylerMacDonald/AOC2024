9c4e0fadd80ee929e24431197dcbfbbfb7b9a60b
PyroPope <tylermacdonald88@gmail.com>
2024-12-02 08:14:59 -0500

    Day 1 + 2 modifiedCurrent

--- current.js
+++ current.js
@@ -4,25 +4,31 @@ const makeKey = (...args)=>{
 };
 var lines = [];
 function parse(text, part2){
+    let levels = [];
     if(isNaN(text)){
         lines = file.getSample(text).trim();
     }else{
         lines = file.getInput(text).trim();
     }
 
-
+    lines = lines.split('\n');
+    //numArray =  lines[i].split(' ').map(x=>Number(x));
     return lines;
 }
 
-
 function solve(part2){
-    let sum = 0;
-    return sum;
+    let count = 0;
+    if(!part2){
+
+    }else{
+
+    }
+    return count;
 }
 
+//parse("sample.txt",false);
+//console.log(parse(1,false))
+let levels = parse("input.txt",false);
 
-//parse(1,false);
-//parse("input.txt",false);
-parse("sample.txt",false);
 console.log(`Part 1: ${solve(false)}`);
 console.log(`Part 2: ${solve(true)}`);
